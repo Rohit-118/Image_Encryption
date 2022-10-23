dc5feae41b7184d68df05860839a067f9237100d
Rohit-118 <rohitnayak118@gmail.com>
2022-10-23 22:55:57 +0530

    Fixed File path

--- SaveImage.java
+++ SaveImage.java
@@ -5,14 +5,14 @@ public class SaveImage {
 
     public static void saveFile(byte[] bytes) throws IOException {
 
-        FileOutputStream fos = new FileOutputStream("C:/Users/KIIT/Desktop/test/test/dec.jpg");
+        FileOutputStream fos = new FileOutputStream("decrypt.jpg");
         fos.write(bytes);
         fos.close();
 
     }
     public static void saveFile2(byte[] bytes) throws IOException {
 
-        FileOutputStream fos = new FileOutputStream("C:/Users/KIIT/Desktop/test/test/enc.jpg");
+        FileOutputStream fos = new FileOutputStream("encrypt.jpg");
         fos.write(bytes);
         fos.close();
     }
